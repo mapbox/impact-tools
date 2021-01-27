f88ec3c74661529fe6348ee109781e16c7503fc4
mjdanielson <megan.danielson@mapbox.com>
2021-01-27 10:46:38 -0800

    Migrate from education repo

--- locator-map/Finder-Template/config.js
+++ locator-map/Finder-Template/config.js
@@ -1,48 +1,60 @@
-
 const config = {
     style: "mapbox://styles/mapbox/light-v10",
-    accessToken: "Your Access Token",
-    CSV: "./Example.csv",
-    center: [-120.234, 47.398], //Lng, Lat
-    zoom: 6, //Default zoom
+    accessToken: "pk.eyJ1IjoibWFwYm94LWNvbW11bml0eSIsImEiOiJja2tkN21jcjAwMG51MnBxdHAxemdueGpzIn0.e0IzLkytGq4pcGGieP8KNA",
+    CSV: "./Sample_Data.csv",
+    center: [-120.234, 47.398],
+    zoom: 6,
     title: "Replace with your title",
     description: "Replace with information about your application. Ex. You can search by address to sort the list below by distance. You can also filter the list by language support options, which days a location is open, and whether they have devices to use to complete the survey by phone or online.",
-    sideBarInfo: ["Header", "Info 1", "Info 2"],
-    popupInfo: ["Popup Information"],
+    sideBarInfo: ["Location_Name", "Address", "Phone"],
+    popupInfo: ["Location_Name"],
     filters: [
         {
             type: "dropdown",
-            title: "Title of filter: ",
-            columnHeader: "Column Name",
+            title: "Languages supported: ",
+            columnHeader: "Languages",
             listItems: [
-                'filter one',
-                'filter two',
-                'filter three',
-                'filter four',
-                'filter five',
-                'filter six',
-                'filter seven'
+                "Amharic",
+                "ASL",
+                "Cambodian",
+                "Chinese",
+                "Danish",
+                "English",
+                "French",
+                "German",
+                "Greek",
+                "Hindi",
+                "Italian",
+                "Japanese",
+                "Korean",
+                "Language Line Services",
+                "Norwegian",
+                "Oriya",
+                "Portuguese",
+                "Punjabi",
+                "Russian",
+                "Somali",
+                "Spanish",
+                "Swedish",
+                "Tagalog",
+                "Thai",
+                "Tigrinya",
+                "Tongan",
+                "Vietnamese",
+                "Ukranian"
             ]
         },
         {
             type: "checkbox",
-            title: "Title of filter: ",
-            columnHeader: "Column Name",
-            listItems: ["filter one", "filter two", "filter three"]
+            title: "Devices available: ",
+            columnHeader: "Devices_available", // Case sensitive - must match spreadsheet entry
+            listItems: ["Computer", "Wi-Fi", "Adaptive Laptops"] // Case sensitive - must match spreadsheet entry; This will take up to six inputs but is best used with a maximum of three;
         },
         {
             type: "dropdown",
-            title: "Title of filter: ",
-            columnHeader: "Column Name",
-            listItems: [
-                'filter one',
-                'filter two',
-                'filter three',
-                'filter four',
-                'filter five',
-                'filter six',
-                'filter seven'
-            ]
+            title: "Clients: ",
+            columnHeader: "Clients",
+            listItems: ["Adults", "Disabled", "Homeless", "Immigrants/Refugees", "Low Income", "Seniors", "Youth: Pre-teen", "Youth: Teen"]
         }
     ]
 
---
+++ spreadsheet-to-map/README.md
@@ -0,0 +1,9 @@
+Create a live-updating map that displays the locations of all your POIs or events, powered by a spreadsheet.
+
+Templates:
+
+[Sample code]()
+[Sample data spreadsheet](https://docs.google.com/spreadsheets/d/1MiqwGe_7m6B0xFQfaS3GGRO8CmGm5xlXPICDPEeGHyo/edit?usp=drive_web&ouid=111368174749056331625)
+[Live map demo](https://labs.mapbox.com/impact-tools/spreadsheet-to-map/)
+
+Check out the [Sheet Mapper tutorial](https://labs.mapbox.com/education/impact-tools/sheet-mapper/) for step by step instructions on how to use this template.
